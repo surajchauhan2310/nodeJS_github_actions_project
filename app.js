9ef176ec282d3ece67d4c90974441b203ff6b6c2
surajchauhan2310 <23surajrc@gmail.com>
2024-09-26 15:06:18 +0530

    moment packaged used to display the time and date as well on the browser

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const moment = require("moment");
 const app = express();
-const date = moment().format("LL");
+const date = moment().format("LLL");
 const PORT = process.env.PORT || 8081;
 
-app.get("/", (req, res) => res.send("Hello World and Hello Juinagar", date));
+app.get("/", (req, res) => res.send("Hello World ===> " + date));
 
 app.listen(PORT, () => {
   console.log("Server listening at Port:", PORT);
