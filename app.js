d61fc8edcc29685370ea37bb02a108318e262430
surajchauhan2310 <23surajrc@gmail.com>
2024-09-26 16:59:51 +0530

    deploying the app using the elastic beanstalk

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8081;
 
 app.get("/", (req, res) => {
   dateTime = moment().format("LLL");
-  res.send("Hello World ==> " + dateTime);
+  res.send("Hello World & Hello Sanpada!!! ==> " + dateTime);
 });
 
 app.listen(PORT, () => {
