7a5742c29ed2e4d366f3e5e0af6e2a1dcbcb72c6
surajchauhan2310 <23surajrc@gmail.com>
2024-09-26 15:12:30 +0530

    updated the code

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const moment = require("moment");
 const app = express();
-const date = moment().format("LLL");
 const PORT = process.env.PORT || 8081;
 
-app.get("/", (req, res) => res.send("Hello World ===> " + date));
+app.get("/", (req, res) => {
+  dateTime = moment().format("LLL");
+  res.send("Hello World ==> " + dateTime);
+});
 
 app.listen(PORT, () => {
   console.log("Server listening at Port:", PORT);
