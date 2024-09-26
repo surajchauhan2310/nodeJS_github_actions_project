819a87a547592fa29fe8c02f7c66e649abd4b997
surajchauhan2310 <23surajrc@gmail.com>
2024-09-26 17:15:32 +0530

    making a health check using the res.status

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8081;
 app.get("/", (req, res) => {
   dateTime = moment().format("LLL");
   res.send("Hello World & Hello Mumbai!!! ==> " + dateTime);
+  res.status(200).send("Application is running on port " + PORT);
 });
 
 app.listen(PORT, () => {
