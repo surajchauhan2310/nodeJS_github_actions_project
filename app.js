37e374137658101b288a91bffe004b17b628dcde
surajchauhan2310 <23surajrc@gmail.com>
2024-09-26 14:50:51 +0530

    commit after the inclusion the moment package to display the date as well

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const moment = require("moment");
 const app = express();
+const date = moment().format("LL");
 const PORT = process.env.PORT || 8081;
 
-app.get("/", (req, res) => res.send("Hello World and Hello Juinagar"));
+app.get("/", (req, res) => res.send("Hello World and Hello Juinagar", date));
 
 app.listen(PORT, () => {
   console.log("Server listening at Port:", PORT);
